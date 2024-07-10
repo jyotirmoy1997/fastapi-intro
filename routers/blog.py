4c89b8830ce178288b41e42bcc0f15fc761a1383
jyotirmoy1997 <subhodas45036@gmail.com>
2024-07-10 09:42:36 +0530

    Added get single blog by id feature

--- routers/blog.py
+++ routers/blog.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status, HTTPException
 from request_schemas import Blog
 from sqlalchemy.orm import Session
 from db import SessionLocal
@@ -13,20 +13,26 @@ def get_db():
     finally:
         db.close()
 
-
+# Getting All Blogs
 @router.get("/")
-def index():
-    return "Index Blog Route"
+def index(db : Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
 
 
+# Getting a single blog by id
 @router.get("/{id}")
-def get_id(id):
-    return {
-        "data" : id
-    }
+def get_blog(id : int, db : Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    # If blog not matches, then we have to throw a 404 not found error
+    if not blog:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = "Blog not found")
 
+    return blog
+    
 
-@router.post("/")
+# Endpoint for creating a new blog
+@router.post("/", status_code = status.HTTP_201_CREATED)
 def post_blog(request : Blog, db : Session = Depends(get_db)):
     new_blog = models.Blog(title = request.title, body = request.body)
     db.add(new_blog)
